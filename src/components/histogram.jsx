414a50a18540ae7ff99752d0b0d75717ea6f3bff
= <matthewcb11@gmail.com>
2019-12-31 13:16:44 -0500

    Fixed insertion sort

--- src/components/histogram.jsx
+++ src/components/histogram.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 import bubbleSort from "../algorithms/bubbleSort";
-import insertionSort from "../algorithms/bubbleSort";
+import insertionSort from "../algorithms/insertionSort";
 import selectionSort from "../algorithms/selectionSort";
 import './histogram.css';
 
